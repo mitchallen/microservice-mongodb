ba59b3821e6ffd4e5078cfe43259535d61d9f8d6
Mitch Allen <mitch.c.allen@gmail.com>
2016-08-08 17:56:44 -0700

    cleaned up examples/music-post

--- README.md
+++ README.md
@@ -122,6 +122,10 @@ Add unit tests for any new or changed functionality. Lint and test your code.
 
 ## Version History
 
+#### Version 0.1.1 release notes
+
+* Cleaned up examples/music-post
+
 #### Version 0.1.0 release notes
 
 * Initial release
--- examples/music-post/index.js
+++ examples/music-post/index.js
@@ -1,62 +1,42 @@
 "use strict";
 
 let demand = require('@mitchallen/demand');
-
-let prefix = process.env.ADMIN_API_VERSION || '/v1';
-
+let prefix = process.env.MUSIC_POST_API_VERSION || '/v1';
 let tableName = "music";
 let path = "/" + tableName;
-
 let sLocation = prefix + path;
 
 var service = {
-    
+
     name: require("./package").name,
     version: require("./package").version,
-
     verbose: true,
-
     apiVersion: prefix,
-
-    port: process.env.TABLE_LIST_PORT || 8003,
-
+    port: process.env.MUSIC_POST_PORT || 8003,
     mongodb: {
         uri: process.env.TEST_MONGO_URI || 'mongodb://localhost/test',
     },
 
     method: function(info) {
-
         var router = info.router,
                db  = info.connection.mongodb.db;
-
         demand.notNull(db);
-
         router.post( path, function (req, res) {
-
-            var collection = db.collection('music');
-
+            var collection = db.collection(tableName);
             // Insert some documents 
-            // In the shell, verify with: db.documents.find()
-
+            // In the mongo shell, verify with: db.music.find()
             collection.insert(
                 req.body
             , function(err, result) {
-
                 if( err ) {
-
                     console.error(err);
                     res
                         .status(500)
                         .send(err);
-
                 } else {
-
                     let docId = result.insertedIds[0];
-
-                    let location = "/v1/music/" + docId;
-
+                    let location = prefix + path + "/" + docId;
                     console.log("New record created: %s", location )
-
                     res
                         .location(location)
                         .status(201)
@@ -69,12 +49,9 @@ var service = {
     }
 };
 
-// TODO - update once module is published
-// module.exports = require('@mitchallen/microservice-mongodb')(service);
-require('../../index')(service, function(err,obj) {
+require('@mitchallen/microservice-mongodb')(service, function(err,obj) {
     if( err ) {
         console.log(err);
         throw new Error( err.message );
     }
-
 });
